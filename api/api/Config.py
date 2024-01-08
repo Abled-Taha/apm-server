36b8232b5fbb741c58cc75dcedba8df5fa5726c3
Abled-Taha <abledtaha@gmail.com>
2024-01-09 04:25:27 +0500

    'Settings.py' now fetcher 'DEBUG' and 'SECRET_KEY' from config.

--- api/api/Config.py
+++ api/api/Config.py
@@ -5,6 +5,9 @@ class Config():
     with open('config/config.json') as configFile:
       self.configData = json.load(configFile)
 
+      self.debug = self.configData['debug']
+      self.secretKey = self.configData['secretKey']
+      
       self.hostDB = self.configData['hostDB']
       self.portDB = self.configData['portDB']
       self.nameDB = self.configData['nameDB']
--- api/api/settings.py
+++ api/api/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from . import Config as ConfigFile
+
+# Setting Up Config
+Config = ConfigFile.Config()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i&5ia(st50%1#r^wju7bx0)w-b-_2v0%gsj#&_71+z_x8lkfvy'
+SECRET_KEY = Config.secretKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = Config.debug
 
 ALLOWED_HOSTS = ["*"]
 
