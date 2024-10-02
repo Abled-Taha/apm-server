8a4e3eacb1ae1b0e79b95062e14f1aeae87baeef
Abled-Taha <abledtaha@gmail.com>
2024-10-02 09:09:05 +0500

    Now the program can be run by waitress using main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+from api.Config import Config as ConfigClass
+
+# Setting up Config
+BASE_DIR = "./api"
+ConfigObj = ConfigClass(BASE_DIR)
+ConfigObj.readConfig()
+
+# Running wsgi server
+os.chdir("./api")
+subprocess.call(["waitress-serve", f"--listen=*:{ConfigObj.server_port}", "api.wsgi:application"])
