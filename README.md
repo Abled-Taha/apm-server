2cffcb0c00ed0f38de2671500cd6815dbb3b265e
Abled-Taha <abledtaha@gmail.com>
2025-12-04 00:33:49 +0500

    Moved secrets into .env and settings into .json

--- README.md
+++ README.md
@@ -14,13 +14,12 @@ The Project [APM-SERVER](https://github.com/Abled-Taha/apm-server), [APM-APP](ht
 2. [Create a user with access of reading and writing to the database](https://www.geeksforgeeks.org/create-user-and-add-role-in-mongodb/)
 3. Clone the repository
 4. Open the directory
-5. Make ".env" file
-6. Set all the variables in that file from the "config.json" file
-7. Open a terminal in "./apm-server"
-8. Run ```pip install -r ./requirements.txt ; cd ./api ; python manage.py collectstatic ; cd .. ; python main.py run-server```
-9. To generate an api token you can use ```python main.py generate-api-token``` which will be used by client
-10. Open http://127.0.0.1:8000/docs and follow the recommendations there to securely setup the API
-11. Restart the API
+5. Rename ".env.example" to ".env"
+6. Open a terminal in "./apm-server"
+7. Run ```pip install -r ./requirements.txt ; cd ./api ; python manage.py collectstatic ; cd .. ; python main.py run-server```
+8. To generate an api token you can use ```python main.py generate-api-token``` which will be used by client
+9. Open http://127.0.0.1:8000/docs and follow the recommendations there to securely setup the API
+10. Restart the API by ```Ctrl + C``` & ```python main.py run-server```
 
 # Documentation
 - For Documentation on [Endpoints](https://github.com/Abled-Taha/apm-server/blob/main/Endpoints.md)
@@ -29,5 +28,4 @@ The Project [APM-SERVER](https://github.com/Abled-Taha/apm-server), [APM-APP](ht
 The same documentaion can be found on the **/docs** endpoint of the API.
 
 # Todo
-1. Add ability to change user credentials
-2. Use the .env file for only some credentials.
+1. Add ability to change user credentials.
--- api/Config.py
+++ api/Config.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 from dotenv import load_dotenv
 
 class Config(object):
@@ -10,42 +10,37 @@ class Config(object):
     try:
       load_dotenv(f'{self.BASE_DIR}/../.env')
     except:
+      print("Warning: .env File Not Found! Trying to load default session values")
       pass
+    try:
+      with open(f'{self.BASE_DIR}/../config.json', 'r') as f:
+        self.config = json.load(f)
+    except:
+      raise Exception("Critical: Config File Not Found!")
       
-    self.debug = os.getenv("debug", "False")
-    if self.debug == "True":
-      self.debug = True
-    else:
-      self.debug = False
-    self.secret_key = os.getenv("secret_key", "django-insecure-czhaxh*di+t*laxo%=bmj8e+8970)#8tgt&77)7&ixd#3j=j=l")
-    self.allowed_hosts = os.getenv("allowed_hosts", "*").split(",")
+    self.secret_key = os.getenv("secret_key", "django-insecure-czhaxh*di+t*laxo%=bmj8e+89708tgt777ixd3jjl")
     self.db_name = os.getenv("db_name", "apm")
     self.db_username = os.getenv("db_username", "apm")
     self.db_host = os.getenv("db_host", "127.0.0.1")
     self.db_port = int(os.getenv("db_port", "27017"))
     self.db_password = os.getenv("db_password", "apmuserpass")
-    self.db_srv = os.getenv("db_srv", "False")
-    if self.db_srv == "True":
-      self.db_srv = True
-    else:
-      self.db_srv = False
-    self.server_host = os.getenv("server_host", "127.0.0.1")
-    self.server_port = int(os.getenv("server_port", "8000"))
-    self.username_min_length = int(os.getenv("username_min_length", "3"))
-    self.username_max_length = int(os.getenv("username_max_length", "10"))
-    self.password_min_length = int(os.getenv("password_min_length", "6"))
-    self.password_max_length = int(os.getenv("password_max_length", "20"))
-    self.sessionId_length = int(os.getenv("sessionId_length", "16"))
-    self.salt_length = int(os.getenv("salt_length", "16"))
-    self.max_sessions = int(os.getenv("max_sessions", "10"))
-    self.pp_width = int(os.getenv("pp_width", "150"))
-    self.pp_height = int(os.getenv("pp_height", "150"))
-    self.email_verification = os.getenv("email_verification", "False")
-    if self.email_verification == "True":
-      self.email_verification = True
-    else:
-      self.email_verification = False
     self.email_host = os.getenv("email_host", "smtp.gmail.com")
     self.email_port = int(os.getenv("email_port", "587"))
     self.email_host_user = os.getenv("email_host_user", "")
     self.email_host_password = os.getenv("email_host_password", "")
+
+    self.debug = self.config["debug"]
+    self.allowed_hosts = self.config["allowed_hosts"]
+    self.db_srv = self.config["db_srv"]
+    self.server_host = self.config["server_host"]
+    self.server_port = self.config["server_port"]
+    self.username_min_length = self.config["username_min_length"]
+    self.username_max_length = self.config["username_max_length"]
+    self.password_min_length = self.config["password_min_length"]
+    self.password_max_length = self.config["password_max_length"]
+    self.sessionId_length = self.config["sessionId_length"]
+    self.salt_length = self.config["salt_length"]
+    self.max_sessions = self.config["max_sessions"]
+    self.pp_width = self.config["pp_width"]
+    self.pp_height = self.config["pp_height"]
+    self.email_verification = self.config["email_verification"]
